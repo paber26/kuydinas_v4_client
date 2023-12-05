9122070b7d3916f29cf99aefe5dd2a0c7df6f5b6
Bernaldo Napitupulu <bernaldonapitupulu@Bernaldos-MacBook-Pro.local>
2023-12-05 17:51:23 +0800

    Perbaikah halaman daftar tryout

--- src/views/TryoutSKDView.vue
+++ src/views/TryoutSKDView.vue
@@ -27,24 +27,24 @@
 
       <div class="col-12">
         <div class="row">
-          <div
-            class="col-xxl-3 col-lg-4 col-md-6 mb-25"
+          <router-link
+            to="/oke"
+            class="col-xxl-3 col-lg-4 col-md-6 mb-15"
             v-for="tskd in tryoutskd"
             :key="tskd.eid"
           >
             <div class="feature-cards feature-card--1">
               <figure class="feather-cards__figure">
-                <figcaption>
+                <figcaption class="p-2">
                   <div class="feature-cards__top">
                     <span></span>
                     <span class="uil uil-pricetag-alt"
                       >{{ tskd.harga }} koin</span
                     >
                   </div>
-                  <h5>{{ tskd.title }}</h5>
+                  <h4>{{ tskd.title }}</h4>
                   <span class="text-xs">
-                    Kerjakan {{ tskd.total }} soal, pengerjaan
-                    {{ tskd.time }} menit
+                    Kerjakan {{ tskd.total }} soal, {{ tskd.time }} menit
                   </span>
                   <div class="feature-cards__bottom">
                     <div class="feedbackSection">
@@ -64,50 +64,7 @@
                 </figcaption>
               </figure>
             </div>
-          </div>
-          <div class="col-xxl-3 col-lg-4 col-md-6 mb-25">
-            <div class="feature-cards feature-card--1">
-              <figure class="feather-cards__figure">
-                <img src="img/blog-single4.png" alt="" />
-                <figcaption>
-                  <div class="feature-cards__top">
-                    <span>Web Development</span><span>01 July 2023 </span>
-                  </div>
-                  <h5>Technology Change the World</h5>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printer took a
-                    galley of type and scrambled
-                  </p>
-                  <div class="feature-cards__bottom">
-                    <div class="feedbackSection">
-                      <div class="author">
-                        <img
-                          class="rounded-circle"
-                          src="img/blog-author.jpg"
-                          alt=""
-                        />
-                        <span>Burns Marks</span>
-                      </div>
-                      <div class="feedback">
-                        <span class="like"
-                          ><img
-                            src="img/svg/heart.svg"
-                            alt="heart"
-                            class="svg"
-                          />
-                          70</span
-                        >
-                        <span class="view"
-                          ><img src="img/svg/eye.svg" alt="eye" class="svg" />
-                          120</span
-                        >
-                      </div>
-                    </div>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
-          </div>
+          </router-link>
         </div>
       </div>
 
